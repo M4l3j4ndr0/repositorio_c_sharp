848191a0abc03b877eb4dbc6f9cb4236f17f09a4
M4l3j4ndr0 <mayeralejandro@gmail.com>
2023-09-02 14:44:14 -0300

    Update actividad clase 1
    finalizado de las actividades adicionales de la clase 1

--- Desafio1_MayerAlejandro/Desafio1_MayerAlejandro/Program.cs
+++ Desafio1_MayerAlejandro/Desafio1_MayerAlejandro/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿// Actividad 1 - Clase 1
 Console.WriteLine("Ingrese su nombre: ");
 string nombre = Console.ReadLine();
 
@@ -17,4 +15,28 @@ int edad = int.Parse(edad_text);
 Console.WriteLine("Ingrese su altura, en metros: ");
 string altura = Console.ReadLine();
 
+Console.WriteLine("Ingrese su apodo: ");
+string apodo = Console.ReadLine();
+
 Console.WriteLine($"Bienvenido {nombre} {apellido} con numero de documento {dni} y con {edad} anios de edad y una altura de {altura} metros   ");
+
+Console.WriteLine("---------------------------------------------------------------------------------------------------------------------------- ");
+
+//Actividad 2 - Clase 1
+Console.WriteLine("Ingrese el número A: ");
+int numero_a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el número B: ");
+int numero_b = int.Parse(Console.ReadLine());
+
+int suma_ab = numero_a + numero_b;
+
+Console.WriteLine($"Los numeros ingresados son {numero_a} & {numero_b} y su suma es: {suma_ab} ");
+
+Console.WriteLine("---------------------------------------------------------------------------------------------------------------------------- ");
+
+//Actividad 3 - Claase 1
+
+string nombre_codigo = nombre + '"' + apodo + '"' + apellido;
+
+Console.WriteLine($"Su nombre secreto es {nombre_codigo}");
