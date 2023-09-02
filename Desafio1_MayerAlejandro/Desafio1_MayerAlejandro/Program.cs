639e3bf8986b27664e2424d58b1ef6ea0c8e3750
M4l3j4ndr0 <mayeralejandro@gmail.com>
2023-09-02 14:27:14 -0300

    Desafio1MayerAlejandro
    ejercicio de practica 1

---
+++ Desafio1_MayerAlejandro/Desafio1_MayerAlejandro/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese su apellido: ");
+string apellido = Console.ReadLine();
+
+Console.WriteLine("Ingrese su dni: ");
+string dni = Console.ReadLine();
+
+Console.WriteLine("Ingrese su edad: ");
+string edad_text = Console.ReadLine();
+int edad = int.Parse(edad_text);
+
+Console.WriteLine("Ingrese su altura, en metros: ");
+string altura = Console.ReadLine();
+
+Console.WriteLine($"Bienvenido {nombre} {apellido} con numero de documento {dni} y con {edad} anios de edad y una altura de {altura} metros   ");
